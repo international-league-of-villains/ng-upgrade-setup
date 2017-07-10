a8bb236a6711a9a8d000a82cc33338c3c3c6cee5
amindunited <buckley.robin@gmail.com>
2017-07-10 21:37:23 +1000

    chore(setup): 6. Add upgraded app to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UpgradeModule } from '@angular/upgrade/static';
+
+import { UpgradedLegacyAppComponent } from './legacy/upgraded-legacy.app';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpgradedLegacyAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  //adding upgrade to the appModule could allow it to be ref'd
+  constructor(public upgrade: UpgradeModule) {}
+  ngDoBootstrap() {}
+}
